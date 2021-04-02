21ee141cf6c2bb1035fa8d46b920c95ffcac4f64
Jan-Luca Klees <janluca@leanatic.com>
2021-04-02 09:29:37 +0200

    Initial commit
    Just set up the basic structure for a Minecraft package.

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Minecraft Plugin: Broadcraft
+
+This is me learning to build minecraft plugins. Especially this is my attempt
+to buid an auto-broadcast system.
---
+++ src/de/janlucaklees/broadcraft/main/Broadcraft.java
@@ -0,0 +1,10 @@
+package de.janlucaklees.broadcraft.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Broadcraft extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
