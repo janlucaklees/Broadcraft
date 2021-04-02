b153b739e3f7e292c79e20b6aa2b7eb8aa25570b
Jan-Luca Klees <janluca@leanatic.com>
2021-04-02 10:02:35 +0200

    Implemented simple broadcasting logic

--- src/de/janlucaklees/broadcraft/main/Broadcraft.java
+++ src/de/janlucaklees/broadcraft/main/Broadcraft.java
@@ -1,10 +1,11 @@
 package de.janlucaklees.broadcraft.main;
 
+import de.janlucaklees.broadcraft.util.Broadcast;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Broadcraft extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        (new Broadcast(this, "Hello", 0, 5*20)).start();
     }
 }
---
+++ src/de/janlucaklees/broadcraft/util/Broadcast.java
@@ -0,0 +1,44 @@
+package de.janlucaklees.broadcraft.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class Broadcast {
+    private final BukkitScheduler scheduler;
+
+    private final JavaPlugin plugin;
+
+    private final String message;
+    private final long delay;
+    private final long interval;
+
+    private int task;
+
+    public Broadcast(JavaPlugin plugin, String message, long delay, long interval) {
+        this.scheduler = Bukkit.getScheduler();
+
+        this.plugin = plugin;
+
+        this.message  = message;
+        this.delay    = delay;
+        this.interval = interval;
+    }
+
+    private Runnable getRunnable(String message) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.getServer().broadcastMessage(message);
+            }
+        };
+    }
+
+    public void start() {
+        this.task = this.scheduler.scheduleSyncRepeatingTask(this.plugin, this.getRunnable(this.message), this.delay, this.interval);
+    }
+
+    public void stop() {
+        this.scheduler.cancelTask(this.task);
+    }
+}
